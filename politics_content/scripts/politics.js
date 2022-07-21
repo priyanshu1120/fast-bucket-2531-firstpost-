abebf7390dea7b7e39b6ef1799a528f73f790225
AmitKuMaurya <amit69maurya69@gmail.com>
2022-07-22 00:40:26 +0530

    politics page almost  final

---
+++ politics_content/scripts/politics.js
@@ -0,0 +1,37 @@
+
+
+let api = `662aa30ca2b1499189f1cbba8cb46f5a`;
+
+import {fetchingApi,appendToHome,fetchApi2,appendToSide} from "./politics_components/fetch&append.js"
+
+let url = `https://newsdata.io/api/1/news?apikey=pub_944869e38eb5301475b41f0b5c3c71340f64&q=politics`;
+
+let data = await fetchingApi(url);
+console.log(data);
+
+let contain = document.getElementById('left-tank'); 
+
+appendToHome(data,contain);
+
+let url2 = `https://newsapi.org/v2/top-headlines?country=in&pageSize=5&apiKey=${api}`  
+ 
+let data2 = await fetchApi2(url2);
+
+let container = document.getElementById('right-tank'); 
+
+appendToSide(data2,container); 
+
+// import {fetchingApi} from "./politics_components/fetch&append.js"
+
+// let url = `https://newsdata.io/api/1/news?apikey=pub_94435419e232c95b6147294a05e7f0cef7b5&q=politics`;
+
+// let data = await fetchingApi(url); 
+
+// console.log(data);
+
+
+
+
+
+
+
