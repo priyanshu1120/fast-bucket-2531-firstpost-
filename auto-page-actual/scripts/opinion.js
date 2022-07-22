62aaf270b4416a810cc9b87dd37a63fb753e7eb5
priyanshu1120 <priyanshukaushik796@gmail.com>
2022-07-22 00:13:45 -0400

    opinion page

---
+++ auto-page-actual/scripts/opinion.js
@@ -0,0 +1,111 @@
+import {
+    subscribe,
+    newsData,
+    newsDisplayData,
+    newsSideDisplayData,
+  } from "../components/fetch.js";
+  
+  let key = "bb025f8cfd2d42748f38ba6ab456f616";
+  let c = "in";
+  let cc = "business";
+  let autoNews = async () => {
+    // let url =`https://newsapi.org/v2/everything?q=general&from=2022-07-19&to=2022-07-19&sortBy=popularity&apiKey=dfec4f3db9ce45279d598a21cd4d6366`
+  
+    let url = `https://newsapi.org/v2/top-headlines?country=${c}&category=${cc}&pageSize=20&apiKey=${key}`;
+    let data = await newsData(url);
+    console.log(data.articles);
+    let container = document.getElementById("newone");
+    container.innerHTML = null;
+    newsDisplayData(data.articles, container, "Opinion","./opinionExplore.html");
+  };
+  
+  autoNews();
+  
+  document.getElementById("subscribe-box").innerHTML = subscribe();
+  let connn = document.getElementById("subscribe_result");
+  // connn.innerHTML = "";
+  let temp1 = document.getElementById("subscribe_btn");
+  temp1.addEventListener("click", () => {
+    let temp2 = document.getElementById("email");
+    event.preventDefault();
+    let h6 = document.createElement("h6");
+    h6.innerText = "Thank you for subscribing!";
+    h6.setAttribute("id", "subscribe_results");
+    temp2.value = null;
+    connn.innerHTML = null;
+    connn.append(h6);
+  });
+  
+  let obj = [
+    {
+      image_url:
+        "https://images.firstpost.com/wp-content/uploads/2022/07/emblem1.jpg?impolicy=website&width=640&height=363",
+      cate: "Opinion",
+      tab: "Should the lions on the emblem look like pet Simbas?",
+    },
+    {
+      image_url:
+        "https://images.firstpost.com/wp-content/uploads/2022/06/namo9.jpg?impolicy=website&width=640&height=362",
+      cate: "Opinion",
+      tab: "First Draft | Time to reclaim India’s history for a New India",
+    },
+    {
+      image_url:
+        "https://images.firstpost.com/wp-content/uploads/2022/07/Brahmastras-Kesariya.jpg?impolicy=website&width=640&height=362",
+      cate: "Opinion",
+      tab: "Not 'Love Storiya' but Instagram reels ruined Brahmastra's Kesariya",
+    },
+  ];
+  
+  slideshow();
+  let i = 0;
+  function slideshow() {
+    let slideshows = document.getElementById("full");
+    setInterval(function () {
+      console.log(i);
+      if (i === obj.length) {
+        i = 0;
+      }
+      let img = document.createElement("img");
+      img.src = obj[i].image_url;
+  
+      let div = document.createElement("div");
+      div.setAttribute("id", "slidebox");
+  
+      let p1 = document.createElement("h3");
+      p1.innerText = obj[i].cate;
+      p1.style.borderBottom = "1px solid #fad65f";
+      p1.style.borderWidth = "5px";
+      p1.style.width = "70px";
+  
+      let p = document.createElement("h2");
+      p.innerText = obj[i].tab;
+  
+      slideshows.innerHTML = null;
+  
+      div.append(p1, p);
+  
+      slideshows.append(img, div);
+      i++;
+    }, 2000);
+  }
+  
+  let sideNews = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&pageSize=5&apiKey=${key}`;
+    let data = await newsData(url);
+    console.log(data.articles);
+    let container = document.getElementById("right_sidebar_news");
+    container.innerHTML = null;
+    newsSideDisplayData(data.articles, container);
+  };
+  sideNews();
+  
+  let refreshPAge = () => {
+    window.location.reload();
+    console.log("abc");
+  };
+  document.getElementById("xyz").addEventListener("click", refreshPAge);
+  document.getElementById("xya").addEventListener("click", refreshPAge);
+  document.getElementById("xyb").addEventListener("click", refreshPAge);
+  document.getElementById("xyc").addEventListener("click", refreshPAge);
+  
---
+++ auto-page-actual/scripts/opinionexplore.js
@@ -0,0 +1,56 @@
+import {
+    subscribe,
+    newsData,
+    newsDisplayData,
+    newsSideDisplayData,
+    explore_details,
+  } from "../components/fetch.js";
+  
+  document.getElementById("subscribe-box").innerHTML = subscribe();
+  let temp1 = document.getElementById("subscribe_btn");
+  temp1.addEventListener("click", () => {
+    let temp2 = document.getElementById("email");
+    event.preventDefault();
+    let h6 = document.createElement("h6");
+    h6.innerText = "Thank you for subscribing!";
+    h6.setAttribute("id", "subscribe_results");
+    temp2.value = null;
+    document.getElementById("subscribe_result").append(h6);
+  });
+  
+  let autoData = JSON.parse(localStorage.getItem("news"));
+  
+  let auto_explore = () => {
+    let container = document.getElementById("newone");
+    explore_details(autoData, container, "Opinion");
+  };
+  auto_explore();
+  
+  let key = "bb025f8cfd2d42748f38ba6ab456f616";
+  let c = "in";
+  let cc = "business";
+  let autoNews = async () => {
+    // let url =`https://newsapi.org/v2/everything?q=general&from=2022-07-19&to=2022-07-19&sortBy=popularity&apiKey=dfec4f3db9ce45279d598a21cd4d6366`
+  
+    let url = `https://newsapi.org/v2/top-headlines?country=${c}&category=${cc}&pageSize=2&apiKey=${key}`;
+  
+    let data = await newsData(url);
+    console.log(data.articles);
+    let container = document.getElementById("relatedNews");
+    container.innerHTML = null;
+    newsDisplayData(data.articles, container, "Opinion");
+  };
+  
+  autoNews();
+  
+  let sideNews = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&pageSize=9&apiKey=${key}`;
+  
+    let data = await newsData(url);
+    console.log(data.articles);
+    let container = document.getElementById("right_sidebar_news");
+    container.innerHTML = null;
+    newsSideDisplayData(data.articles, container);
+  };
+  sideNews();
+  
